2b8b73e3b82c86f1ff1451e919caeb70ae4fa654
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-04-08 22:24:58 +0900

    feat: 아웃풋 뷰 구현

---
+++ src/main/java/lotto/view/outputView.java
@@ -0,0 +1,30 @@
+package lotto.view;
+
+import lotto.model.Lotto;
+
+import java.util.List;
+import java.util.Map;
+
+public class outputView {
+    public void printLotto(int money, List<Lotto> lottos){
+        System.out.println(money/1000+"개를 입력했습니다.");
+        for (int i = 0; i < lottos.size(); i++) {
+            System.out.println("로또 " + (i + 1) + "번: " + lottos.get(i).getNumbers());
+        }
+    }
+
+    public void printResult(Map<String, Integer> result){
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+
+        System.out.println("3개 일치 (5,000원) - " + result.get("3개") + "개");
+        System.out.println("4개 일치 (50,000원) - " + result.get("4개") + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + result.get("5개") + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + result.get("5개,보너스") + "개");
+        System.out.println("6개 일치 (200,000,000원) - " + result.get("6개") + "개");
+    }
+
+    public void printProfitRate(double profitRate){
+        System.out.println("총 수익률은 "+profitRate+"입니다.");
+    }
+}
