7871cf4b115e464214e9beb436d2f8e519facbdd
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-04-08 15:35:25 +0900

    feat: Lotto클래스 구현 및 정적 팩토리 메서드 구현

---
+++ src/main/java/lotto/model/Lottos.java
@@ -0,0 +1,24 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static Lottos from(int num){
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            lottoList.add(new Lotto());
+        }
+        return new Lottos(lottoList);
+    }
+
+    public List<Lotto> getLottos(){
+        return lottos;
+    }
+}
