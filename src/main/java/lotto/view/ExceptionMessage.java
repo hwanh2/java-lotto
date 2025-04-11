913fd3eca34fe08f81650558b3c97280e925870b
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-04-11 12:04:07 +0900

    refactor: ExceptionMessage클래스를 enum으로 변경

--- src/main/java/lotto/view/ExceptionMessage.java
+++ src/main/java/lotto/view/ExceptionMessage.java
@@ -1,42 +1,25 @@
 package lotto.view;
 
-public class ExceptionMessage {
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
-    private static final String NOT_NUMBER_RANGE = "[ERROR] 숫자는 " + MIN_NUMBER + "부터 " + MAX_NUMBER + " 사이의 숫자여야 합니다.";
-    private static final String NOT_NUMBER_SIZE = "[ERROR] 당첨 번호는 6개 입력 가능합니다.";
-    private static final String NOT_NUMBER_OVERLAP = "[ERROR] 중복된 숫자를 입력하셨습니다.";
-    private static final String INPUT_TYPE_ERROR = "[ERROR] 숫자만 입력해 주세요.";
-    private static final int LOTTO_MIN_AMOUNT = 1000;
-    private static final String NOT_NUMBER_ERROR = "[ERROR] 금액은 숫자만 등록 가능합니다.";
-    private static final String NOT_MINIMUM_AMOUNT_ERROR = "[ERROR] 금액은 1000원 이상이어야 합니다.";
-    private static final String NOT_DIVISIBLE_NUMBER_ERROR = "[ERROR] 금액은 " + LOTTO_MIN_AMOUNT + "단위여야 합니다.";
+public enum ExceptionMessage {
+    NOT_NUMBER_RANGE("[ERROR] 숫자는 1부터 45 사이의 숫자여야 합니다."),
+    NOT_NUMBER_SIZE("[ERROR] 당첨 번호는 6개 입력 가능합니다."),
+    NOT_NUMBER_OVERLAP("[ERROR] 중복된 숫자를 입력하셨습니다."),
+    INPUT_TYPE_ERROR("[ERROR] 숫자만 입력해 주세요."),
+    NOT_NUMBER_ERROR("[ERROR] 금액은 숫자만 등록 가능합니다."),
+    NOT_MINIMUM_AMOUNT_ERROR("[ERROR] 금액은 1000원 이상이어야 합니다."),
+    NOT_DIVISIBLE_NUMBER_ERROR("[ERROR] 금액은 1000단위여야 합니다.");
 
-    public static void rangeException() {
-        System.out.println(NOT_NUMBER_RANGE);
+    private final String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
     }
 
-    public static void sizeException() {
-        System.out.println(NOT_NUMBER_SIZE);
+    public void print() {
+        System.out.println(message);
     }
 
-    public static void overlapException() {
-        System.out.println(NOT_NUMBER_OVERLAP);
-    }
-
-    public static void typeException() {
-        System.out.println(INPUT_TYPE_ERROR);
-    }
-
-    public static void numberException() {
-        System.out.println(NOT_NUMBER_ERROR);
-    }
-
-    public static void minimumException() {
-        System.out.println(NOT_MINIMUM_AMOUNT_ERROR);
-    }
-
-    public static void divisibleException() {
-        System.out.println(NOT_DIVISIBLE_NUMBER_ERROR);
+    public String getMessage() {
+        return message;
     }
 }
