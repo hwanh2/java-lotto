7ed271833d6c0cce619522baf2c164122eed40df
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-04-24 22:17:22 +0900

    fix: 테스트 코드 수정

--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -1,5 +1,8 @@
 package lotto.model;
 
+import lotto.generator.NumberGenerator;
+import lotto.generator.RandomNumberGenerator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -7,13 +10,21 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottosTest {
+
+    NumberGenerator generator;
+
+    @BeforeEach
+    void setUp() {
+        generator = new RandomNumberGenerator();
+    }
+
     @Test
     void 지정한_개수의_Lotto_객체를_생성한다() {
         // given
         int count = 8;
 
         // when
-        Lottos lottos = Lottos.from(count);
+        Lottos lottos = Lottos.from(count, generator);
         List<Lotto> lottoList = lottos.getLottos();
         for (int i = 0; i < lottoList.size(); i++) {
             System.out.println("로또 " + (i + 1) + "번: " + lottoList.get(i).getNumbers());
