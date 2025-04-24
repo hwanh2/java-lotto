b878ab4c983c4615bb77ce3f1839b91b04215c6f
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-04-24 23:20:15 +0900

    refactor: 테스트에 고정값 넣어서 테스트

--- src/test/java/lotto/service/ServiceTest.java
+++ src/test/java/lotto/service/ServiceTest.java
@@ -1,6 +1,31 @@
 package lotto.service;
 
+import lotto.generator.FixNumberGenerator;
+import lotto.model.LottoRank;
+import lotto.model.Lottos;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ServiceTest {
 
+    @Test
+    void 로또_한장이_몇등인지_확인한다() {
+        // given
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6); // 당첨 번호
+        int bonusNumber = 7;
 
+        FixNumberGenerator generator = new FixNumberGenerator(Arrays.asList(1, 2, 3, 4, 5, 7));
+        LottoService service = new LottoService();
+
+        Lottos lottos = service.createLottos(1000, generator);
+
+        // when
+        Map<LottoRank, Integer> result = service.matchLotto(lottos, winningNumbers, bonusNumber);
+
+        // then
+        assertThat(result.get(LottoRank.FIVE)).isEqualTo(1); // 5개 일치
+    }
 }
